10f73d705806a94307ad7ff09f3c764108e9fa42
David Kemp <davidbkemp@gmail.com>
2025-04-05 17:35:18 +1100

    Implement neighboursOfCell

--- Cephalopods.kt
+++ Cephalopods.kt
@@ -18,6 +18,7 @@ fun main(args: Array<String>) {
 
     System.err.println("Depth is: $depth")
     System.err.println("Initial board: ${initialBoard}")
+    testNeighboursOfCell()
 
     println(initialBoard.solve(depth))
 }
@@ -43,6 +44,15 @@ data class Board(val cells: IntArray) {
         return Board(newCells).solve(depth - 1)
     }
 
+/** 
+    fun capturesForCell(cell: Int): List<List<Int>> {
+        val canditates = neighboursOfCell(cell).filter {
+            it > 0 && it < 6
+        }
+        val combos = combinations(candidates)
+    }
+*/
+
     fun isComplete(): Boolean = cells.all { it > 0 }
 
     fun boardHash(): Int = cells.fold(0) { acc, x -> addHashes(10 * acc, x) }
@@ -51,3 +61,20 @@ data class Board(val cells: IntArray) {
 }
 
 fun addHashes(h1: Int, h2: Int): Int = (h1 + h2) % (1 shl 30)
+
+fun neighboursOfCell(cell: Int): List<Int> {
+    val result = ArrayList<Int>()
+    if (cell >= BOARD_WIDTH ) result.add(cell - BOARD_WIDTH)
+    if (cell < BOARD_SIZE - BOARD_WIDTH) result.add(cell + BOARD_WIDTH)
+    if (cell % BOARD_WIDTH > 0) result.add(cell - 1)
+    if (cell % BOARD_WIDTH < BOARD_WIDTH - 1) result.add(cell + 1)
+    return result
+}
+
+fun testNeighboursOfCell() {
+    if (neighboursOfCell(0).toSet() != listOf(1, 3).toSet()) throw IllegalStateException("neighboursOfCell(0) was ${neighboursOfCell(0)}")
+    if (neighboursOfCell(1).toSet()  != listOf(0, 2, 4).toSet() ) throw IllegalStateException("neighboursOfCell(1) was ${neighboursOfCell(1)}")
+    if (neighboursOfCell(2).toSet()  != listOf(1, 5).toSet() ) throw IllegalStateException("neighboursOfCell(2) was ${neighboursOfCell(2)}")
+    if (neighboursOfCell(4).toSet()  != listOf(1, 3, 5, 7).toSet() ) throw IllegalStateException("neighboursOfCell(4) was ${neighboursOfCell(4)}")
+    if (neighboursOfCell(8).toSet()  != listOf(7, 5).toSet() ) throw IllegalStateException("neighboursOfCell(8) was ${neighboursOfCell(8)}")
+}
