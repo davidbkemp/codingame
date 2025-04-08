ea26463ed87c90b3dfed54621e04e278bf3414a8
David Kemp <davidbkemp@gmail.com>
2025-04-08 23:23:46 +1200

    Use unsigned int for cell values and board hashes
    Didn’t really seem to make a lot of difference

--- Cephalopods.kt
+++ Cephalopods.kt
@@ -17,24 +17,18 @@ fun main(args: Array<String>) {
     val input = Scanner(System.`in`)
     val depth = input.nextInt()
 
-    val initialBoard = Board(IntArray(BOARD_SIZE) { _ -> input.nextInt() })
+    val initialBoard = Board(UIntArray(BOARD_SIZE) { _ -> input.nextInt().toUInt() })
 
     System.err.println("Depth is: $depth")
     System.err.println("Initial board: ${initialBoard}")
-//    testNeighboursOfCell()
-//    testCombinations()
-//    testMovesForCell()
-  //  testHash()
- //   testAllZeros()
-
 
     val result = initialBoard.solve(depth).mod(1u shl 30)
     println(result)
 }
 
-data class Move(val cell: Int, val captures: List<Int>, val value: Int)
+data class Move(val cell: Int, val captures: List<Int>, val value: UInt)
 
-data class Board(val cells: IntArray) {
+data class Board(val cells: UIntArray) {
 
     fun solve(depth: Int): UInt {
         val cacheKey = Pair(depth, this.hash)
@@ -43,10 +37,10 @@ data class Board(val cells: IntArray) {
         } else if (solutionCache.containsKey(cacheKey)) {
             return solutionCache[cacheKey]!!
         } else {
-            val emptyCells = (0 until cells.size).filter { cells[it] == 0 }
+            val emptyCells = (0 until cells.size).filter { cells[it] == 0u }
             val result: UInt =
                     emptyCells.fold(0u) { acc, cell ->
-                        addHashes(acc, solveForCell(depth, cell))
+                        acc + solveForCell(depth, cell)
                     }
             solutionCache[cacheKey] = result
             return result
@@ -56,7 +50,7 @@ data class Board(val cells: IntArray) {
     fun solveForCell(depth: Int, cell: Int): UInt {
         return movesForCell(cell).fold(0u) { acc, move ->
             val newBoard = applyMove(move)
-            addHashes(acc, newBoard.solve(depth - 1))
+            acc + newBoard.solve(depth - 1)
         }
     }
 
@@ -64,7 +58,7 @@ data class Board(val cells: IntArray) {
         val newCells = cells.copyOf()
         newCells[move.cell] = move.value
         move.captures.forEach {
-            newCells[it] = 0
+            newCells[it] = 0u
         }
         return Board(newCells)
     }
@@ -72,56 +66,25 @@ data class Board(val cells: IntArray) {
 
     fun movesForCell(cell: Int): List<Move> {
         val candidates = neighboursOfCell(cell).filter {
-            cells[it] > 0 && cells[it] < 6
+            cells[it] > 0u && cells[it] < 6u
         }
         val capturingMoves = combinations(candidates)
             .map {
                 Move(cell, it, it.map{cells[it]}.sum())
             }
-            .filter { it.captures.size > 1 && it.value <= 6 }
+            .filter { it.captures.size > 1 && it.value <= 6u }
         if (capturingMoves.size == 0) {
-            return listOf(Move(cell, emptyList(), 1))
+            return listOf(Move(cell, emptyList(), 1u))
         } else {
             return capturingMoves
         }
     }
 
-    fun isComplete(): Boolean = cells.all { it > 0 }
+    fun isComplete(): Boolean = cells.all { it > 0u }
 
-    val hash: UInt = cells.fold(0u) { acc, x -> addHashes(10u * acc, x.toUInt()) }
+    val hash: UInt = cells.fold(0u) { acc, x -> 10u * acc + x}
 
     override fun toString(): String = cells.toList().toString()
-}
-
-inline fun addHashes(h1: UInt, h2: UInt): UInt = (h1 + h2)// % (1 shl 30)
-
-fun testHash() {
-    val b1 = Board(intArrayOf(1,2,3,1,2,3,1,2,3))
-    val b2 = Board(intArrayOf(1,2,3,1,2,3,1,2,3))
-    if (b1.hash != b2.hash) throw IllegalStateException("hash ${b1.hash}")
-
-}
-
-fun testMovesForCell() {
-    val moves = Board(intArrayOf(0, 1, 0, 2, 0, 3, 0, 4, 0)).movesForCell(4)
-    val actualCaptures = moves.map { it.captures.toSet() }.toSet()
-   System.err.println("moves $moves")
-    val expectedCaptures = setOf(
-        setOf(1, 3),
-        setOf(1, 5),
-        setOf(1, 7),
-        setOf(1, 3, 5),
-        setOf(3, 5),
-        setOf(3, 7)
-
-    )
-    val actualMinusExpected = actualCaptures - expectedCaptures
-    val expectedMinusActual = expectedCaptures - actualCaptures
-    if (actualMinusExpected.size != 0) throw IllegalStateException("capturesForCell actualMinusExpected $actualMinusExpected")
-    if (expectedMinusActual.size != 0) throw IllegalStateException("capturesForCell expectedMinusActual $expectedMinusActual")
-    val actualValues = moves.map { it.value }.toSet()
-    val expectedValues = setOf(3,4,5,6)
-    if (!actualValues.equals(expectedValues)) throw IllegalStateException("move values ${actualValues}")
 }
 
 fun neighboursOfCell(cell: Int): List<Int> {
@@ -131,14 +94,6 @@ fun neighboursOfCell(cell: Int): List<Int> {
     if (cell % BOARD_WIDTH > 0) result.add(cell - 1)
     if (cell % BOARD_WIDTH < BOARD_WIDTH - 1) result.add(cell + 1)
     return result
-}
-
-fun testNeighboursOfCell() {
-    if (neighboursOfCell(0).toSet() != listOf(1, 3).toSet()) throw IllegalStateException("neighboursOfCell(0) was ${neighboursOfCell(0)}")
-    if (neighboursOfCell(1).toSet()  != listOf(0, 2, 4).toSet() ) throw IllegalStateException("neighboursOfCell(1) was ${neighboursOfCell(1)}")
-    if (neighboursOfCell(2).toSet()  != listOf(1, 5).toSet() ) throw IllegalStateException("neighboursOfCell(2) was ${neighboursOfCell(2)}")
-    if (neighboursOfCell(4).toSet()  != listOf(1, 3, 5, 7).toSet() ) throw IllegalStateException("neighboursOfCell(4) was ${neighboursOfCell(4)}")
-    if (neighboursOfCell(8).toSet()  != listOf(7, 5).toSet() ) throw IllegalStateException("neighboursOfCell(8) was ${neighboursOfCell(8)}")
 }
 
 fun combinations(items: List<Int>): List<List<Int>> {
@@ -152,34 +107,4 @@ fun combinations(items: List<Int>): List<List<Int>> {
             .plus(tailCombinations)
             .plus(tailCombinations.map{it.plus(head)})
     }
-}
-
-
-fun testCombinations() {
-    val actual: Set<Set<Int>> = combinations(listOf(1,2,3,4)).map{it.toSet()}.toSet()
-    val expected: Set<Set<Int>> = setOf(
-            setOf(1,2,3,4),
-            setOf(1,2,3),
-            setOf(1,2,4),
-            setOf(1,3,4),
-            setOf(2,3,4),
-            setOf(1,2),
-            setOf(1,3),
-            setOf(1,4),
-            setOf(2,3),
-            setOf(2,4),
-            setOf(3,4),
-            setOf(1),
-            setOf(2),
-            setOf(3),
-            setOf(4)
-            )
- //       System.err.println("actual - expected: ${actual - expected}")
- //       System.err.println("expected: $expected")
-        if (actual != expected) throw IllegalStateException("Combos was ${combinations(listOf(1,2,3,4))}")
-}
-
-fun testAllZeros() {
-    val board = Board(intArrayOf(6,6,6,6,6,6,6,6,0))
-    System.err.println(board.solve(10))
 }
